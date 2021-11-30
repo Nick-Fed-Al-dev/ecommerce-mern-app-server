ba9b1337248c3d8785709b8af462e469ebff234c
Nick-Fed-Al-dev <fna.1011@gmail.com>
2021-11-30 21:11:39 +0900

    auth revision

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const config = require('config')
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 const PORT = config.get('port') || 3030
