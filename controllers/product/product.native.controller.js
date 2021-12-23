0ab35f9a96f9a46f0a3cee78b132ec86beaea705
Nick-Fed-Al-dev <fna.1011@gmail.com>
2021-12-23 22:05:31 +0900

    products query filtration

--- controllers/product/product.native.controller.js
+++ controllers/product/product.native.controller.js
@@ -4,79 +4,15 @@ class ProductNativeController{
 
   async getProducts(req, res){
     try {
-      const products = await Product.find({})
-      // await (new Product({
-      //   title: 'Tablet product1',
-      //   price: 1100,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
-      // await (new Product({
-      //   title: 'Tablet product2',
-      //   price: 1450,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
-      // await (new Product({
-      //   title: 'Tablet product3',
-      //   price: 4100,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
-      // await (new Product({
-      //   title: 'Tablet product4',
-      //   price: 4700,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
-      // await (new Product({
-      //   title: 'Tablet product5',
-      //   price: 1500,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
-      // await (new Product({
-      //   title: 'Tablet product6',
-      //   price: 3670,
-      //   type: 'Tablets',
-      //   description: 'Best product of the best Best product of the best Best product of the best Best product of the best Best product of the best Best product of the bestvBest product of the bestvBest product of the best Best product of the best Best product of the best Best product of the best',
-      //   properties: [
-      //     {type: 'Speed', value: '23km/h'},
-      //     {type: 'Width', value: '100m'},
-      //     {type: 'Length', value: '50m'},
-      //     {type: 'USB', value: 'Exists'}
-      //   ]
-      // })).save()
+      const page = req.query.page || 1
+      const category = req.query.category
+      const products = await Product.find(category ? {type: category} : {})
+      products = products.filter((product, i) => {
+        if(i === 1){
+          return (i >= page*10-10-1) && (i < page*10)
+        }
+        return (i > page*10-10-1) && (i < page*10)
+      })
       res.json(products)
     } catch (error) {
       
